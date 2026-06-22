92c1c9d875d247eabc22d672147b5090a3c6cca6
aarondeloach <aarondeloach@gmail.com>
2026-06-22 17:22:12 -0500

    Add "PRE:" prefix to data display in +page.svelte for clarity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<pre>{JSON.stringify(data, null, 2)}</pre>
+<pre>PRE: {JSON.stringify(data, null, 2)}</pre>
