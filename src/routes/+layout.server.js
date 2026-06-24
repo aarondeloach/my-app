cf084063617a41b3acbdb6cf150e306d9e876450
aarondeloach <aarondeloach@gmail.com>
2026-06-24 09:03:18 -0500

    Add database queries for tables, users, and accounts in +layout.server.js

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -9,6 +9,9 @@ export async function load({ locals }) {
         dbStatus: await db.execute("SHOW STATUS WHERE Variable_name IN ('Uptime', 'Threads_connected', 'Threads_running', 'Max_used_connections')").then(([rows]) => rows.reduce((acc, row) => {
             acc[row.Variable_name] = row.Value;
             return acc;
-        }, {}))
+        }, {})),
+        dbTables: await db.execute("SHOW TABLES").then(([rows]) => rows.map(row => Object.values(row)[0] || [])),
+        dbUsers: await db.execute("SELECT id, uuid, name, email, account_id, status, roles FROM users").then(([rows]) => rows || []),
+        dbAccounts: await db.execute("SELECT id, uuid, title FROM accounts").then(([rows]) => rows || []),
     };
 }
